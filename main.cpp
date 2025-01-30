c4edc88293ea99b61eba2796f19b0c891edb8583
pashka <pashka@DESKTOP-31L8IQM>
2025-01-30 19:16:39 +0300

    сортировка

--- main.cpp
+++ main.cpp
@@ -1,33 +1,89 @@
 #include <iostream>
 #include <Windows.h>
 
-int foo(int a, int b)
+
+// Сортировка пузырьком
+void bubbleSort(int arr[], int size)
 {
-	if (b == 0)
-	{
-		return 0;
-	}
-	if (b < 0)
-	{
-		return -(a + foo(a, -(b) - 1));
-	}
-	return a + foo(a, b - 1);
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    std::cout << "Сортировка пузырьком: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
 }
 
-int main()
+// Сортировка выбором
+void selectionSort(int arr[], int size)
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	
-	int number1, number2;
-
-	std::cout << "введите первое число: ";
-	std::cin >> number1;
-	std::cout << "введите второе число: ";
-	std::cin >> number2;
-
-	std::cout << foo(number1, number2);
-
-
-	return 0;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minId = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minId])
+            {
+                minId = j;
+            }
+        }
+        std::swap(arr[i], arr[minId]);
+    }
+    std::cout << "Сортировка выбором: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
 }
+
+
+// Сортировка вставками
+void insertionSort(int arr[], int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    std::cout << "Сортировка вставками: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 5;
+    int arrBubble[size] = { 5, 2, 4, 3, 1 };
+    int arrInsertion[size] = { 4, 1, 2, 5, 3 };
+    int arrSelection[size] = { 3, 1, 4, 5, 2 };
+
+    bubbleSort(arrBubble, size);
+
+    selectionSort(arrSelection, size);
+
+    insertionSort(arrInsertion, size);
+
+    return 0;
+}
+
+
